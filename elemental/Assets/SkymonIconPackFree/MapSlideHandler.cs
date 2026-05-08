eb6cbe3520f8f21eaeb0d6b134339ed5314e53f0
241036kuga <c241036@ivy.ac.jp>
2026-05-08 14:22:56 +0900

    おそらくカメラが動くスクリプト

---
+++ elemental/Assets/SkymonIconPackFree/MapSlideHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MapSlideHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
