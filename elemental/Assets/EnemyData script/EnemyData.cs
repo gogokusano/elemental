930e060dc11d83f0ba797e7c76dfae57e7f9f950
241012 <gogokusano.ivy.githab.io>
2026-05-29 13:47:23 +0900

    行動予測の追加と敵AIの強化

--- elemental/Assets/EnemyData script/EnemyData.cs
+++ elemental/Assets/EnemyData script/EnemyData.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// 敵の行動の種類を増やす
+// 敵の行動の種類
 public enum EnemyActionType { Attack, Defend, AddStatusCard }
 
 [System.Serializable]
@@ -10,7 +10,14 @@ public class EnemyAction
     public string actionName;
     public EnemyActionType actionType;
     public int value; // ダメージやブロックの数値
-    public CardData statusCard; // ★追加：AddStatusCardの時に付与するカード
+    public CardData statusCard; // AddStatusCardの時に付与するカード
+
+    [Header("AI条件設定")]
+    [Tooltip("チェックを入れると、敵のHPが50%以下の時しか使ってこない大技になります")]
+    public bool isPhase2Only = false; 
+
+    [Tooltip("チェックを入れると、戦闘中に1回しか使ってこなくなります（バフや特殊行動など）")]
+    public bool isOneTimeOnly = false;
 }
 
 [CreateAssetMenu(fileName = "NewEnemy", menuName = "Enemy/EnemyData")]
@@ -21,6 +28,6 @@ public class EnemyData : ScriptableObject
     public int maxHP;
     public Sprite enemyImage;
 
-    [Header("行動リスト (ランダムで選ばれます)")]
+    [Header("行動リスト")]
     public List<EnemyAction> actionList = new List<EnemyAction>();
 }
